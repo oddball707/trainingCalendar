295fdfa10f6e0f5cda127ea29ee744e5ce082612
Eli Moskowitz <emoskowitz@circadence.com>
2019-01-08 16:03:11 -0800

    start with only local files

--- cal.go
+++ cal.go
@@ -2,119 +2,116 @@ package main
 
 import (
 	"encoding/json"
+	"encoding/csv"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
 	"time"
-
-	"github.com/jordic/goics"
-	"golang.org/x/net/context"
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/google"
-	
-	s "google.golang.org/api/sheets/v4"
 
 )
 
 const dateLayout = "01/02/2006"
+const file = "calendar.csv"
 
-// Retrieve a token, saves the token, then returns the generated client.
-func getClient(config *oauth2.Config) *http.Client {
-	// The file token.json stores the user's access and refresh tokens, and is
-	// created automatically when the authorization flow completes for the first
-	// time.
-	tokFile := "token.json"
-	tok, err := tokenFromFile(tokFile)
-	if err != nil {
-		tok = getTokenFromWeb(config)
-		saveToken(tokFile, tok)
-	}
-	return config.Client(context.Background(), tok)
+// Event is a time entry
+type Event struct {
+	DateStart   time.Time `json:"dateStart"`
+	DateEnd     time.Time `json:"dateEnd"`
+	Description string    `json:"description"`
 }
 
-// Request a token from the web, then returns the retrieved token.
-func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
-	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
-	fmt.Printf("Go to the following link in your browser then type the "+
-		"authorization code: \n%v\n", authURL)
-
-	var authCode string
-	if _, err := fmt.Scan(&authCode); err != nil {
-		log.Fatalf("Unable to read authorization code: %v", err)
-	}
-
-	tok, err := config.Exchange(context.TODO(), authCode)
-	if err != nil {
-		log.Fatalf("Unable to retrieve token from web: %v", err)
-	}
-	return tok
+type Week struct {
+	weekStart	time.Time   `json:"weekStart"`
+	monday		string 		`json:"monday"`
+	tuesday		string 		`json:"tuesday"`
+	wednesday	string 		`json:"wednesday"`
+	thursday	string 		`json:"thursday"`
+	friday		string 		`json:"friday"`
+	saturday	string 		`json:"saturday"`
+	sunday		string 		`json:"sunday"`
 }
 
-// Retrieves a token from a local file.
-func tokenFromFile(file string) (*oauth2.Token, error) {
-	f, err := os.Open(file)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-	tok := &oauth2.Token{}
-	err = json.NewDecoder(f).Decode(tok)
-	return tok, err
-}
+// EmitICal implements the interface for goics
+func (e Event) EmitICal(c goics.Componenter) {
+	c.AddProperty("CALSCAL", "GREGORIAN")
+	s := goics.NewComponent()
+	s.SetType("VEVENT")
+	k, v := goics.FormatDateTimeField("DTEND", e.DateEnd)
+	s.AddProperty(k, v)
+	k, v = goics.FormatDateTimeField("DTSTART", e.DateStart)
+	s.AddProperty(k, v)
+	s.AddProperty("SUMMARY", e.Description)
 
-// Saves a token to a file path.
-func saveToken(path string, token *oauth2.Token) {
-	fmt.Printf("Saving credential file to: %s\n", path)
-	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
-	if err != nil {
-		log.Fatalf("Unable to cache oauth token: %v", err)
-	}
-	defer f.Close()
-	json.NewEncoder(f).Encode(token)
+	c.AddComponent(s)
 }
 
 
+func reschedule(f *os.File, raceDate time.Time) {
+	writer := csv.NewWriter(file)
+    defer writer.Flush()
 
-func reschedule(srv *s.Service, raceDate time.Time) {
+    weeks := loadCalendar(f)
+
+    
 
 }
 
-func createIcal(srv *s.Service) {
-	c := goics.NewComponent()
-	c.SetType("VCALENDAR")
+func create(f *os.File) {
+
+
+}
+
+func loadCalendar(f *os.File) []Week {
+	// Read File into a Variable
+    lines, err := csv.NewReader(f).ReadAll()
+    if err != nil {
+        panic(err)
+    }
+
+    var sched []Week
+
+    // Loop through lines & turn into object
+    for _, line := range lines {
+        wk := Week{
+            weekStart: line[0],
+            monday: line[1],
+         	tuesday: line[2],	
+			wednesday: line[3],
+			thursday: line[4],
+			friday: line[5],
+			saturday: line[6],
+			sunday: line[7]
+        }
+        sched = append(sched, wk)
+    }
+
+    return sched
 }
 
 func main() {
 
 	recalcDate := flag.String("date", "", "Recalculate schedule based on date of race passed in (mm/dd/yyyy)")
 	create := flag.Bool("create", false, "Create a new ical")
+	filestr := flag.String("file", "", "csv file to use")
 	flag.Parse()
 
 
-	b, err := ioutil.ReadFile("credentials.json")
-	if err != nil {
-		log.Fatalf("Unable to read client secret file: %v", err)
+	if(*filestr != "") {
+		file = *filestr
 	}
 
-	// If modifying these scopes, delete your previously saved token.json.
-	config, err := google.ConfigFromJSON(b, "https://www.googleapis.com/auth/spreadsheets.readonly")
-	if err != nil {
-		log.Fatalf("Unable to parse client secret file to config: %v", err)
-	}
-	client := getClient(config)
-
-	srv, err := s.New(client)
-	if err != nil {
-		log.Fatalf("Unable to retrieve Sheets client: %v", err)
-	}
+		// Open CSV file
+    f, err := os.Open(file)
+    if err != nil {
+        panic(err)
+    }
+    defer f.Close()
 
 	if( *recalcDate == "") {
 		createIcal(srv);
 	} else if(*create) {
-
 		raceDate, err := time.Parse(dateLayout, *recalcDate)
 		if err != nil {
 			log.Fatalf("Improperly formated date: " + *recalcDate)
