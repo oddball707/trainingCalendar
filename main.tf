7274c446db247bb8c57d53de2eb1fbd76b423242
Eli Moskowitz <emoskowitz@aspiration.com>
2023-02-16 10:31:29 -0800

    use s3 for lambda zip

--- main.tf
+++ main.tf
@@ -21,12 +21,38 @@ provider "aws" {
   region  = "us-west-2"
 }
 
+
+data "archive_file" "zip" {
+  type        = "zip"
+  source_file = "lambda/main"
+  output_path = "main.zip"
+}
+
+resource "random_pet" "lambda_bucket_name" {
+  prefix = "training-calendar"
+  length = 4
+}
+
+resource "aws_s3_bucket" "lambda_bucket" {
+  bucket = random_pet.lambda_bucket_name.id
+}
+
+resource "aws_s3_object" "lambda_training_calendar" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+
+  key    = "main.zip"
+  source = data.archive_file.zip.output_path
+
+  etag = filemd5(data.archive_file.zip.output_path)
+}
+
 resource "aws_lambda_function" "training-calendar-generator" {
   function_name    = "time"
-  filename         = "lambda/lambda.zip"
+  s3_bucket        = aws_s3_bucket.lambda_bucket.id
+  s3_key           = aws_s3_object.lambda_training_calendar.key
   handler          = "main"
   source_code_hash = "data.archive_file.zip.output_base64sha256"
-  role             = "${aws_iam_role.iam_for_lambda.arn}"
+  role             = aws_iam_role.iam_for_lambda.arn
   runtime          = "go1.x"
   memory_size      = 128
   timeout          = 10
@@ -35,21 +61,18 @@ resource "aws_lambda_function" "training-calendar-generator" {
 resource "aws_iam_role" "iam_for_lambda" {
   name = "iam_for_lambda"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      }
+    ]
+  })
 }
 
 resource "aws_api_gateway_rest_api" "api" {
@@ -71,7 +94,7 @@ resource "aws_api_gateway_method" "method" {
 
 resource "aws_amplify_app" "training-calendar-frontend" {
   name       = "Training Calendar Frontend"
-  repository = "github.com/oddball707/trainingCalendar/app"
+  repository = "github.com/oddball707/trainingCalendar"
   iam_service_role_arn = aws_iam_role.iam_for_lambda.arn
   enable_branch_auto_build = true
   build_spec = <<-EOT
@@ -80,17 +103,19 @@ resource "aws_amplify_app" "training-calendar-frontend" {
       phases:
         preBuild:
           commands:
+            - cd app
             - npm install
         build:
           commands:
+            - cd app
             - npm run build
       artifacts:
-        baseDirectory: build
+        baseDirectory: app/build
         files:
           - '**/*'
       cache:
         paths:
-          - node_modules/**/*
+          - app/node_modules/**/*
   EOT
   # The default rewrites and redirects added by the Amplify Console.
   custom_rule {
