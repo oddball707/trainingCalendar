9666e4eaa5963fc71f5b3b61a17c76d026e9da3d
Eli Moskowitz <emoskowitz@aspiration.com>
2023-02-22 16:16:15 -0800

    Fix typo

--- main.tf
+++ main.tf
@@ -80,7 +80,7 @@ resource "aws_api_gateway_resource" "proxy" {
 
 resource "aws_api_gateway_method" "method" {
   rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
-  resource_id   = "${aws_api_gateway_resource.resource.id}"
+  resource_id   = "${aws_api_gateway_resource.proxy.id}"
   http_method   = "ANY"
   authorization = "NONE"
 }
