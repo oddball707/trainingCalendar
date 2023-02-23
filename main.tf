46022480df0cc67945c6e56b92731ed5aae9bdff
Eli Moskowitz <emoskowitz@aspiration.com>
2023-02-22 16:15:02 -0800

    Add lambda integration

--- main.tf
+++ main.tf
@@ -51,12 +51,29 @@ resource "aws_iam_role" "iam_for_lambda" {
 EOF
 }
 
+# Attach role to Managed Policy
+variable "iam_policy_arn" {
+  description = "IAM Policy to be attached to role"
+  type        = list(string)
+
+  default = [
+    "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  ]
+}
+
+resource "aws_iam_policy_attachment" "role_attach" {
+  name       = "policy-lambda"
+  roles      = [aws_iam_role.iam_for_lambda.id]
+  count      = length(var.iam_policy_arn)
+  policy_arn = element(var.iam_policy_arn, count.index)
+}
+
 resource "aws_api_gateway_rest_api" "api" {
   name = "training_cal_api"
 }
 
-resource "aws_api_gateway_resource" "resource" {
-  path_part   = "generator"
+resource "aws_api_gateway_resource" "proxy" {
+  path_part   = "{proxy+}"
   parent_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
   rest_api_id = "${aws_api_gateway_rest_api.api.id}"
 }
@@ -64,6 +81,48 @@ resource "aws_api_gateway_resource" "resource" {
 resource "aws_api_gateway_method" "method" {
   rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
   resource_id   = "${aws_api_gateway_resource.resource.id}"
-  http_method   = "POST"
+  http_method   = "ANY"
   authorization = "NONE"
+}
+
+resource "aws_api_gateway_method" "proxy_root" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_rest_api.api.root_resource_id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_method.method.resource_id
+  http_method             = aws_api_gateway_method.method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.training-calendar-generator.invoke_arn
+}
+
+resource "aws_api_gateway_integration" "integration_root" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_method.proxy_root.resource_id
+  http_method             = aws_api_gateway_method.proxy_root.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.training-calendar-generator.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "api_deployment" {
+  depends_on = [
+    aws_api_gateway_integration.integration,
+    aws_api_gateway_integration.integration_root,
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name  = "api"
+}
+
+resource "aws_lambda_permission" "lambda_permission" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.training-calendar-generator.arn
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_deployment.api_deployment.execution_arn}/*/*"
 }
