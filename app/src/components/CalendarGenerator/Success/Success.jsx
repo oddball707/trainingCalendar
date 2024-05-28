6b200fb0f3b9d0d76874f27f75cc5d819604310b
Eli Moskowitz <oddball707@gmail.com>
2024-05-28 11:46:31 -0700

    Start over button cleanup

--- app/src/components/CalendarGenerator/Success/Success.jsx
+++ app/src/components/CalendarGenerator/Success/Success.jsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import { Button, Typography } from '@material-ui/core';
+import { Box, Button, Typography } from '@material-ui/core';
 
 function Success() {
   return (
     <React.Fragment>
-      <Typography variant="h5" gutterBottom>
-        Your ical training calendar should begin downloading shortly
-      </Typography>
-      <Typography variant="subtitle1">
-        Thank you for using this tool!
-      </Typography>
-      <Button
-        onClick={() => {
-          location.reload();
-        }}
-        variant="contained"
-        color="primary"
-      >
-        Back
-      </Button>
+      <Box textAlign='center'>
+        <Typography variant="h5" gutterBottom>
+          Your ical training calendar should begin downloading shortly
+        </Typography>
+        <Typography variant="subtitle1">
+          Thank you for using this tool!
+        </Typography>
+        <br/>
+
+        <Button
+          onClick={() => {
+            location.reload();
+          }}
+          variant="contained"
+          color="primary"
+        >
+          Start Over
+        </Button>
+      </Box>
     </React.Fragment>
   );
 }
