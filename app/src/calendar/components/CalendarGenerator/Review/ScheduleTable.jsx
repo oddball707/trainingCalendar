8cfc19d236b6e16b316721bdafbf4e12ba032a3e
Eli Moskowitz <oddball707@gmail.com>
2025-09-11 11:31:27 -0700

    Schedule page visual improvements

--- app/src/calendar/components/CalendarGenerator/Review/ScheduleTable.jsx
+++ app/src/calendar/components/CalendarGenerator/Review/ScheduleTable.jsx
@@ -54,43 +54,56 @@ class ScheduleTable extends React.Component {
   render() {
     const { tableData } = this.state;
     const classes = useStyles();
+    const raceType = this.props.formValues?.raceType;
     return (
-      <TableContainer component={Paper}>
-        <Table size="large" aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Week</TableCell>
-              <TableCell>Date</TableCell>
-              <TableCell align="right">Monday</TableCell>
-              <TableCell align="right">Tuesday</TableCell>
-              <TableCell align="right">Wednesday</TableCell>
-              <TableCell align="right">Thursday</TableCell>
-              <TableCell align="right">Friday</TableCell>
-              <TableCell align="right">Saturday</TableCell>
-              <TableCell align="right">Sunday</TableCell>
-              <TableCell align="right">Weekly Distance</TableCell>
-              <TableCell align="right">Increase over Previous Week</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {(Array.isArray(tableData) ? tableData : []).map((week, weekNumber) => (
-              <TableRow key={weekNumber}>
-                <TableCell component="th" scope="row">
-                  {weekNumber + 1}
-                </TableCell>
-                <TableCell component="th" scope="row">
-                  {moment(new Date(week.weekStart)).format('M/DD')}
-                </TableCell>
-                {(Array.isArray(week.days) ? week.days : []).map((day, idx) => (
-                  <TableCell align="right" key={idx}>{day.description}</TableCell>
-                ))}
-                <TableCell component="th" scope="row">{week.totalDistance}</TableCell>
-                <TableCell component="th" scope="row">{week.wowIncrease}</TableCell>
+      <>
+        <TableContainer component={Paper}>
+          <Table size="large" aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Week</TableCell>
+                <TableCell>Date</TableCell>
+                <TableCell align="center">Monday</TableCell>
+                <TableCell align="center">Tuesday</TableCell>
+                <TableCell align="center">Wednesday</TableCell>
+                <TableCell align="center">Thursday</TableCell>
+                <TableCell align="center">Friday</TableCell>
+                <TableCell align="center">Saturday</TableCell>
+                <TableCell align="center">Sunday</TableCell>
+                {raceType !== '1' && <TableCell align="center">Weekly Distance</TableCell>}
+                {raceType !== '1' && <TableCell align="center">Increase over Previous Week</TableCell>}
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+            </TableHead>
+            <TableBody>
+              {(Array.isArray(tableData) ? tableData : []).map((week, weekNumber) => (
+                <TableRow key={weekNumber} sx={week.wowIncrease === '-' && weekNumber > 1 ? { backgroundColor: '#575757ff' } : {}}>
+                  <TableCell component="th" scope="row">
+                    {weekNumber + 1}
+                  </TableCell>
+                  <TableCell component="th" scope="row">
+                    {moment(new Date(week.weekStart)).format('M/DD')}
+                  </TableCell>
+                  {(Array.isArray(week.days) ? week.days : []).map((day, idx) => (
+                    <TableCell align="center" key={idx}>{day.description}</TableCell>
+                  ))}
+                  {raceType !== '1' && (
+                    <TableCell  align="center" component="th" scope="row">{week.totalDistance}</TableCell>
+                  )}
+                  {raceType !== '1' && (
+                    <TableCell  align="center" component="th" scope="row">{week.wowIncrease}</TableCell>
+                  )}
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        {raceType !== '1' && (
+          <div style={{ marginTop: 16, display: 'flex', alignItems: 'center' }}>
+            <div style={{ width: 24, height: 24, backgroundColor: '#575757ff', border: '1px solid #575757ff', marginRight: 8 }} />
+            <Typography variant="body2">Rest/Taper week</Typography>
+          </div>
+        )}
+      </>
     );
   }
 }
